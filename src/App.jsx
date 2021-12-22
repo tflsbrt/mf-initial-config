7c411b3ca4d92d3207b025971787adb3d32c6b29
thais.felisberto <thais.felisberto@i-value.com.br>
2021-12-21 21:20:19 -0300

    Initial config

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <>
+      <h1>Leads Management Microfrontend</h1>
+    </>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
+module.exports = () => {
+  const isDevelopment = process.env.NODE_ENV !== 'production';
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.jsx', '.js'],
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'public'),
+      hot: true,
+      host: '0.0.0.0',
+      port: 5001,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      },
+    },
+    plugins: [
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new WebpackManifestPlugin({
+        fileName: 'asset-manifest.json',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'public/manifest.json', to: 'manifest.json' },
+          { from: 'public/robots.txt', to: 'robots.txt' },
+        ],
+      }),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    externals: ['single-spa'],
+  };
+};
