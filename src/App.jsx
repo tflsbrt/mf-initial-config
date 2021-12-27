79d70c3e76ce84ce0515858e2822c2ad404da02d
thais.felisberto <thais.felisberto@i-value.com.br>
2021-12-27 12:30:58 -0300

    Adds styles for mf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import "./styles/global.scss";
+
 export function App() {
   return (
     <>
       <h1>Leads Management Microfrontend</h1>
+      <button onClick={() => alert('Hi there. This is a microfrontends proof of concept.')}>Click Here</button>
     </>
   );
 }
